28464b24c629ab6dab5adb537b4de9bd67f22c83
Christan Coskey <christan1234@gmail.com>
2025-06-13 14:24:28 +0200

    update theme styling and add input

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,45 @@
 import "@mantine/core/styles.css";
-import { Button } from "@mantine/core";
+import { AppShell, Burger, CloseButton, Group } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import "./global.css";
+import { Input } from "@mantine/core";
+import { useState } from "react";
 
 export default function App() {
-  return <Button variant="filled">Button</Button>;
+  const [opened, { toggle }] = useDisclosure();
+  const [value, setValue] = useState("Clear me");
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header h={"100px"}>
+        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        <Group justify="center" align="center" h={"100px"}>
+          <Input
+            w={"50%"}
+            placeholder="Search for artist"
+            value={value}
+            onChange={(event) => setValue(event.currentTarget.value)}
+            rightSectionPointerEvents="all"
+            rightSection={
+              <CloseButton
+                aria-label="Search for artist"
+                onClick={() => setValue("")}
+                style={{ display: value ? undefined : "none" }}
+              />
+            }
+          />
+        </Group>
+      </AppShell.Header>
+
+      <AppShell.Main></AppShell.Main>
+    </AppShell>
+  );
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,46 @@
-import { createTheme } from '@mantine/core';
+import { createTheme, Input, colorsTuple, CloseButton } from '@mantine/core';
 
 export const theme = createTheme({
+
+  breakpoints: {
+    xs: '30em',
+    sm: '48em',
+    md: '64em',
+    lg: '74em',
+    xl: '90em',
+    xxl: '120em'
+  },
+ components: {
+
+  Input: Input.extend({
+    styles: {
+      input: {
+        color: 'white',
+        letterSpacing: '0.5px',
+        borderColor: '#F0F0F3',
+        background: 'var(--mantine-color-searchGrey-0)',
+        height: '50px',
+      },
+
+    }
+
+  }),
+
+  CloseButton: CloseButton.extend({
+    styles: {
+      root: {
+        color: 'white',
+        backgroundColor: 'var(--mantine-color-searchGrey-0)',
+    }}
+
+  }),
+
+
+
+ },
+
+ colors: {
+  searchGrey: colorsTuple('#243E51'),
+ },
+
 });
