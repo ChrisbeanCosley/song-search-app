eca5f7910bceecfc5075cc2ec70a180ca2b99459
Christan Coskey <christan1234@gmail.com>
2025-06-13 15:09:34 +0200

    Add styling and functionality to search bar and card for displaying albums

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,77 @@
 import "@mantine/core/styles.css";
-import { AppShell, Burger, CloseButton, Group } from "@mantine/core";
+import {
+  AppShell,
+  Burger,
+  Button,
+  Card,
+  CloseButton,
+  Grid,
+  Group,
+  Image,
+  Title,
+} from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import "./global.css";
 import { Input } from "@mantine/core";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { IconSearch } from "@tabler/icons-react";
+
+const CLIENT_ID = "0982dc44ffdf4b95a3df245151f16360";
+const ClIENT_SECRET = "d39b260f1bad4a0496062d67f6f99a2a";
 
 export default function App() {
   const [opened, { toggle }] = useDisclosure();
-  const [value, setValue] = useState("Clear me");
+  const [searchInput, setSearchInput] = useState("");
 
   return (
-    <AppShell
-      header={{ height: 60 }}
-      navbar={{
-        width: 300,
-        breakpoint: "sm",
-        collapsed: { mobile: !opened },
-      }}
-      padding="md"
-    >
+    <AppShell header={{ height: 60 }} padding="md">
       <AppShell.Header h={"100px"}>
         <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
         <Group justify="center" align="center" h={"100px"}>
           <Input
             w={"50%"}
             placeholder="Search for artist"
-            value={value}
-            onChange={(event) => setValue(event.currentTarget.value)}
+            value={searchInput}
+            onChange={(event) => setSearchInput(event.currentTarget.value)}
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                console.log("Search for:", searchInput);
+              }
+            }}
             rightSectionPointerEvents="all"
+            leftSection={<IconSearch size={16} />}
             rightSection={
               <CloseButton
                 aria-label="Search for artist"
-                onClick={() => setValue("")}
-                style={{ display: value ? undefined : "none" }}
+                onClick={() => setSearchInput("")}
+                style={{ display: searchInput ? undefined : "none" }}
               />
             }
           />
+          <Button
+            onClick={() => {
+              console.log("clicked button");
+            }}
+          >
+            Search
+          </Button>
         </Group>
       </AppShell.Header>
 
-      <AppShell.Main></AppShell.Main>
+      <AppShell.Main>
+        <Grid bg={"#162733"} mt={"xl"} justify="left">
+          <Grid.Col span={4}>
+            <Card shadow="sm" radius="md">
+              <Card.Section>
+                <Image src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png" />
+              </Card.Section>
+              <Card.Section>
+                <Title>Album Name Here</Title>
+              </Card.Section>
+            </Card>
+          </Grid.Col>
+        </Grid>
+      </AppShell.Main>
     </AppShell>
   );
 }
